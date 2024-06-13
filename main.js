839ee6819f2f1e521d7419f1bae686891571365b
Ravi Raj <39424997+ravirajcool54@users.noreply.github.com>
2024-06-13 17:53:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const navlist = document.getElementById('navlist');
+const hamburger = document.getElementById('hamburger');
+
+// Click Event 
+
+hamburger.addEventListener('click', ()=>{
+
+  navlist.classList.toggle('navlist-active')
+
+
+})
